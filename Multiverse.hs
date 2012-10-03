bbd5c89b62273bc1450a6c8dd61749a8f83f1951
Tom Hulihan <hulihan.tom159@gmail.com>
2012-10-02 20:46:07 -0400

    greatly improved the server, particularly in the command line and department

--- Multiverse.hs
+++ Multiverse.hs
@@ -1,17 +1,50 @@
+import Data.List
+import Data.Word
+import Data.Maybe
 import System
 import System.IO
+import System.Directory
 import Network
 import Network.Socket hiding (accept)
 import Control.Monad
 
+-- Process the arguments, and obtain a port and output file from them. If none
+-- exist, default arguments are supplied (stdout for the file and 8808 for the
+-- port).
 main :: IO ()
-main = do
-    fileName <- fmap head getArgs
-    file <- openFile fileName AppendMode
-    putStrLn "listening on port 8808, press Ctrl+C to stop"
-    forever $ logClientMessage (PortNumber 8808) file
+main = do (port, handle) <- processArgs
+          forever $ logClientMessage port handle
 
-logClientMessage ::  PortID -> Handle -> IO ()
+processArgs :: IO (PortID, Handle)
+processArgs = do
+    args   <- getArgs
+    port   <- case map fromJust . filter isJust $ map getPort args of
+                  []    -> putStrLn "No port supplied (-p), using 8808."
+                        >> return (PortNumber 8808)
+                  (p:_) -> p
+    handle <- case map fromJust . filter isJust $ map getFile args of
+                  []    -> putStrLn "No file supplied (-f), using stdout."
+                        >> return stdout
+                  (f:_) -> f
+    return (port, handle)
+
+getPort :: String -> Maybe (IO PortID)
+getPort (    '-':'p':                port) = Just . return . PortNumber $ fromIntegral (read port :: Int)
+getPort ('-':'-':'p':'o':'r':'t':'=':port) = Just . return . PortNumber $ fromIntegral (read port :: Int)
+getPort _                                  = Nothing
+
+getFile :: String -> Maybe (IO Handle)
+getFile (    '-':'f':                                fileName) = Just $ openFileForLogging fileName
+getFile ('-':'-':'f':'i':'l':'e':'n':'a':'m':'e':'=':fileName) = Just $ openFileForLogging fileName
+getFile _                                                      = Nothing
+
+openFileForLogging :: FilePath -> IO Handle
+openFileForLogging fileName = do
+    exists <- doesFileExist fileName
+    openFile fileName (if exists then AppendMode
+                                 else WriteMode)
+
+logClientMessage :: PortID -> Handle -> IO ()
 logClientMessage port outH = withSocketsDo $ do
     socket           <- listenOn port
     (inH, _, _)      <- accept socket
@@ -20,5 +53,3 @@ logClientMessage port outH = withSocketsDo $ do
     hFlush outH
     hClose inH
     sClose socket
-
-
