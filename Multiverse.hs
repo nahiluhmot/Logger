11a58607a799e8bf31b5000eaff0d777d50f9014
Tom Hulihan <hulihan.tom159@gmail.com>
2012-10-06 17:30:49 -0400

    made the command line processing use Parsec

--- Multiverse.hs
+++ Multiverse.hs
@@ -1,51 +1,48 @@
-{-# LANGUAGE FlexibleInstances #-}
-
-import Default
-import Data.List
-import Data.Word
-import Data.Maybe
-import System
-import System.IO
-import System.Directory
+import Control.Monad (forever)
 import Network
-import Network.Socket hiding (accept)
-import Control.Monad
+import System (getArgs)
+import System.IO
+import System.Directory (doesFileExist)
+import Text.ParserCombinators.Parsec
 
 -- Process the arguments, and obtain a port and output file from them. If none
 -- exist, default arguments are supplied (stdout for the file and 8808 for the
 -- port).
 main :: IO ()
-main = do args   <- getArgs
-          port   <- mapRun getPort args
-          handle <- mapRun getFile args
-          forever $ logClientMessage port handle
+main = withSocketsDo $ do 
+           args   <- getArgs
+           handle <- doParseAll parseFile args (return stdout)
+           port   <- doParseAll parsePort args (return (PortNumber 8808))
+           socket <- listenOn port
+           forever $ logClientMessage socket handle
+           sClose socket
 
-instance Default (IO Handle) where
-    defaultValue = return stdout
-instance Default (IO PortID) where
-    defaultValue = return (PortNumber 8808)
+doParseAll :: Parser a -> [String] -> a -> a
+doParseAll _ []     def = def
+doParseAll p (x:xs) def = case parse p "" x of
+                               Left _  -> doParseAll p xs def
+                               Right y -> y
 
-getPort :: String -> Maybe (IO PortID)
-getPort (    '-':'p':                port) = Just . return . PortNumber $ fromIntegral (read port :: Int)
-getPort ('-':'-':'p':'o':'r':'t':'=':port) = Just . return . PortNumber $ fromIntegral (read port :: Int)
-getPort _                                  = Nothing
-
-getFile :: String -> Maybe (IO Handle)
-getFile (    '-':'f':                                fileName) = Just $ openFileForLogging fileName
-getFile ('-':'-':'f':'i':'l':'e':'n':'a':'m':'e':'=':fileName) = Just $ openFileForLogging fileName
-getFile _                                                      = Nothing
+parsePort :: Parser (IO PortID)
+parsePort = do { string "-p" <|> string "--portname="
+               ; p <- many1 digit
+               ; return . return . PortNumber $ fromIntegral (read p :: Int)
+               }
+parseFile :: Parser (IO Handle)
+parseFile = do { string "-f" <|> string "--filename="
+               ; f <- many1 anyChar
+               ; return (openFileForLogging f)
+               }
 
 openFileForLogging :: FilePath -> IO Handle
 openFileForLogging fileName = do
     exists <- doesFileExist fileName
     openFile fileName (if exists then AppendMode else WriteMode)
 
-logClientMessage :: PortID -> Handle -> IO ()
-logClientMessage port outH = withSocketsDo $ do
-    socket           <- listenOn port
+--logClientMessage :: PortID -> Handle -> IO ()
+logClientMessage socket outH =  do
     (inH, _, _)      <- accept socket
     m                <- hGetContents inH
     hPutStrLn outH m
     hFlush outH
     hClose inH
-    sClose socket
